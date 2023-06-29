7cf0d2a9d307d07bb12c3f8165d8315fb95216fe
Sorach-Roshe <olanike20@gmail.com>
2023-06-29 20:59:20 +0100

    changed the jpeg to png

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ import json
 #st.set_page_config(page_title="TechIntel Tweet Classifier App")
 from PIL import Image
 # Loading Image using PIL
-pic = Image.open('resources/TwitIntel.jpeg')
+pic = Image.open('resources/TwitIntel.png')
 # Adding Image to web app
 st.set_page_config(page_title="TwitIntel", page_icon = pic)
 
